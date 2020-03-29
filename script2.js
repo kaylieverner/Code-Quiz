6deb06075bb4d73b1e034676d35d6091c8d9107a
Kaylie V <k.verner37@gmail.com>
2020-03-28 23:15:33 -0400

    updates

--- script2.js
+++ script2.js
@@ -67,14 +67,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var questionCounter = 0;
     var score = 0;
-     var questionScreen = document.querySelector(".questionScreen")
+    var questionScreen = document.querySelector(".questionScreen")
 
     startBtn.addEventListener("click", renderQuestion);
 
 
     function renderQuestion() {
 
-       
+
         var questionDiv = document.createElement("div");
         var question = document.createElement("h2");
         var buttonDiv = document.createElement("div");
@@ -115,36 +115,79 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log(questionCounter);
             resultText.innerHTML = "Correct"
         } else {
-            questionCounter++
             resultText.innerHTML = "Incorrect"
-        }; 
+            questionCounter++
+
+        };
 
         // check if we've run out of questions    
         if (questionCounter == 4) {
-            renderEnd; 
+            renderEnd();
         } else {
             renderQuestion();
         };
     };
 
-    function renderEnd () {
-        alert("end"); 
-    }; 
+    function renderEnd() {
+        questionScreen.innerHTML = "";
+        var endMessage = document.createElement("h1");
+        var scoreResult = document.createElement("h4");
+        var inputField = document.createElement("input");
+        var inputBtn = document.createElement("button");
 
-    //Upon clicking an answer option: 
+        questionScreen.appendChild(endMessage);
+        questionScreen.appendChild(scoreResult);
+        questionScreen.appendChild(inputField);
+        questionScreen.appendChild(inputBtn);
 
-    //If correct answer is selected, display the message "correct" and increase score 
+        inputBtn.classList.add("btn", "btn-outline-secondary", "inputBtnClass");
+        inputBtn.textContent = "Submit Name";
 
-    //If incorrect answer is selected, display the message "incorrect"
+        endMessage.textContent = "Quiz Completed!"
+        scoreResult.textContent = "You got " + score + " of 4 questions correct";
 
-    //Advance on to the next question and answer set 
+        inputBtn.addEventListener("click", function (event) {
+            event.preventDefault();
 
-    //Upon answering the last question: 
+            var playerScore = {
+                name: inputField.value.trim(),
+                scoreLog: score,
+            };
 
-    //Display the score/result to the user 
+            console.log(playerScore);
 
-    //Prompt an input box to add the user's name to the highscores leaderboard 
+            localStorage.setItem("playerScore", JSON.stringify("playerScore"));
 
-    //Provide options to return to main page?
+            var highscoreDiv = document.createElement("div");
+            var highscoreDivName = document.createElement("div");
+            var highscoreDivScore = document.createElement("div");
+            var highscoreName = document.createElement("p");
+            var highscoreScore = document.createElement("p");
+
+            questionScreen.appendChild(highscoreDiv);
+            highscoreDiv.appendChild(highscoreDivName);
+            highscoreDiv.appendChild(highscoreDivScore);
+            highscoreDivName.appendChild(highscoreName);
+            highscoreDivScore.appendChild(highscoreScore);
+
+            highscoreDiv.classList.add("row");
+            highscoreDivName.classList.add("col-6");
+            highscoreDivScore.classList.add("col-6");
+
+            endMessage.textContent = "Highscores";
+            scoreResult.textContent = "";
+            inputBtn.classList.add("hide");
+            inputField.classList.add("hide");
+
+            highscoreName.textContent = playerScore.name;
+            highscoreScore.textContent = playerScore.scoreLog;
+
+            var playAgainBtn = document.createElement("button");
+
+
+        });
+    };
+
+
 
 });
